8655829b4188c4dc6a7275c58a992750e8b615b4
Admin <admin@example.com>
2026-07-07 14:46:02 +0800

    隐藏登录页面右上角的布局切换按钮

--- apps/web-antd/src/layouts/auth.vue
+++ apps/web-antd/src/layouts/auth.vue
@@ -18,6 +18,7 @@ const logoDark = computed(() => preferences.logo.sourceDark);
     :logo-dark="logoDark"
     :page-description="$t('authentication.pageDesc')"
     :page-title="$t('authentication.pageTitle')"
+    :toolbar-list="['color', 'language', 'theme']"
   >
     <!-- 自定义工具栏 -->
     <!-- <template #toolbar></template> -->
